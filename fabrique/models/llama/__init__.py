aa80a6946071f0113ba3175ec2c34d41e7febe59
Andrei Zhabinski <faithlessfriend@gmail.com>
2024-06-23 16:06:07 +0000

    Fix tests, formatting

--- fabrique/models/llama/__init__.py
+++ fabrique/models/llama/__init__.py
@@ -1,11 +1,10 @@
 from fabrique.loading import LoadConfig
-from fabrique.models.llama.model import ModelArgs, Transformer
 from fabrique.models.llama.load_rules import RULES
-
+from fabrique.models.llama.model import ModelArgs, Transformer
 
 LOAD_CONFIG = LoadConfig(
     model_types=["llama"],
     model_args_class=ModelArgs,
     model_class=Transformer,
-    rules=RULES
+    rules=RULES,
 )
--- fabrique/models/phi/__init__.py
+++ fabrique/models/phi/__init__.py
@@ -1,11 +1,10 @@
 from fabrique.loading import LoadConfig
-from fabrique.models.phi.model import ModelArgs, Transformer
 from fabrique.models.phi.load_rules import RULES
-
+from fabrique.models.phi.model import ModelArgs, Transformer
 
 LOAD_CONFIG = LoadConfig(
     model_types=["phi3"],
     model_args_class=ModelArgs,
     model_class=Transformer,
-    rules=RULES
+    rules=RULES,
 )
